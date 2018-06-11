bb3071d05a11c0ad1f9f60adf22cf1ddff8c54cc
mayu-ot <otani.mayu.ob9@is.naist.jp>
2018-06-11 19:08:03 +0900

    prepare script

--- codes/script/shell/prepare_eval.sh
+++ codes/script/shell/prepare_eval.sh
@@ -6,14 +6,18 @@ echo wea val
 
 echo "all single multiple" | xargs -n 1 -r -P 3 python codes/script/preprocess/precompute_affinity_matrix.py  --pair_score_file models/wea/res_val.csv --method wea --split val --eval_type
 
-echo ${DIR} test
+echo wea test
 echo "all single multiple" | xargs -n 1 -r -P 3 python codes/script/preprocess/precompute_affinity_matrix.py  --pair_score_file models/wea/res_test.csv --method wea --split test --eval_type
 
 for DIR in `ls models`
 do
+echo evaluate model ${DIR}
+python codes/script/training/train_paraphrase_classifier.py -d 0 --eval models/${DIR}/
+done
 
+for DIR in `ls models`
+do
 echo ${DIR} val
-
 echo "all single multiple" | xargs -n 1 -r -P 3 python codes/script/preprocess/precompute_affinity_matrix.py  --pair_score_file models/${DIR}/res_val.csv --method ${DIR} --split val --eval_type
 
 echo ${DIR} test
